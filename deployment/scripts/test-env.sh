8016d4852a593930428138fa8312c041b874c427
Nikita Yugov <nikita@elastoo.com>
2020-06-12 20:36:50 +0700

    Added raise error in test-env.sh

--- deployment/scripts/test-env.sh
+++ deployment/scripts/test-env.sh
@@ -19,14 +19,14 @@ echo "########## Testing env variables ##########"
 ##  test -n "$var" || echo Fatal : missing env "var"
 #done
 
-test -n "$ENV_BACKEND" || echo Fatal : missing file ENV_BACKEND
-test -n "$ENV_FRONTEND" || echo Fatal : missing file ENV_FRONTEND
-test -n "$ENV_DB" || echo Fatal : missing file ENV_DB
-test -n "$ENV_RABBITMQ" || echo Fatal : missing file ENV_DB
-test -n "$SSH_USER" || echo Fatal : missing variable SSH_USER
-test -n "$SSH_HOST" || echo Fatal : missing variable SSH_HOST
-test -n "$SSH_PRIVATE_KEY" || echo Fatal : missing variable SSH_PRIVATE_KEY
-test -n "$SSH_KNOWN_HOSTS" || echo Fatal : missing variable SSH_KNOWN_HOSTS
-test -n "$PROJECT_DIR" || echo Fatal : missing variable PROJECT_DIR
-test -n "$DOCKER_COMPOSE_FILENAME" || echo Fatal : missing variable DOCKER_COMPOSE_FILENAME
-test -n "$CI_ENVIRONMENT_SLUG" || echo Fatal : missing variable CI_ENVIRONMENT_SLUG
+test -n "$ENV_BACKEND" || echo Fatal : missing file ENV_BACKEND && exit 1
+test -n "$ENV_FRONTEND" || echo Fatal : missing file ENV_FRONTEND && exit 1
+test -n "$ENV_DB" || echo Fatal : missing file ENV_DB && exit 1
+test -n "$ENV_RABBITMQ" || echo Fatal : missing file ENV_RABBITMQ && exit 1
+test -n "$SSH_USER" || echo Fatal : missing variable SSH_USER && exit 1
+test -n "$SSH_HOST" || echo Fatal : missing variable SSH_HOST && exit 1
+test -n "$SSH_PRIVATE_KEY" || echo Fatal : missing variable SSH_PRIVATE_KEY && exit 1
+test -n "$SSH_KNOWN_HOSTS" || echo Fatal : missing variable SSH_KNOWN_HOSTS && exit 1
+test -n "$PROJECT_DIR" || echo Fatal : missing variable PROJECT_DIR && exit 1
+test -n "$DOCKER_COMPOSE_FILENAME" || echo Fatal : missing variable DOCKER_COMPOSE_FILENAME && exit 1
+test -n "$CI_ENVIRONMENT_SLUG" || echo Fatal : missing variable CI_ENVIRONMENT_SLUG && exit 1
