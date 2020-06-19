10e569f692e6df9c47830675a858cc13f91b565e
saint777 <saint7794@gmail.com>
2020-06-19 19:26:23 +0300

    feat: pipeline update

--- frontend/pages/exchange/index.vue
+++ frontend/pages/exchange/index.vue
@@ -1,15 +1,29 @@
 <template lang="pug">
   div
     div.main-content
-      n-link(to="/exchange/buysell").btn.mr-2 Buy
-      n-link(to="/exchange/buysell").btn.mr-2 Sell
-      b-table(:data="data" :columns="columns" focusable striped).is-flex.mt-4.data-table
+      div
+        n-link(to="/exchange/buysell").btn.mr-2 Buy
+        n-link(to="/exchange/buysell").btn.mr-2 Sell
+      h3.title.is-5.mt-4 Last bills
+      //= b-table(:data="data" :columns="columns" focusable striped).is-flex.mt-4.data-table
+      b-table(:data="invoiceData")
+        template(slot-scope="props")
+          b-table-column(field="created_at" label="Date" sortable) {{ (new Date(props.row.created_at)).toLocaleString() }}
+          b-table-column(field="id" label="ID")
+              n-link(:to="'/exchange/' + props.row._id") {{ props.row._id }}
+          b-table-column(field="simba_amount" label="SIMBA Amount") {{ props.row.simba_amount }}
+          b-table-column(field="invoice_type" label="Type") {{ getType(props.row.invoice_type) }}
 </template>
 
 <script>
   export default {
     name: "exchange-index",
     layout: 'main',
+    computed: {
+      invoiceData() {
+        return this.$store.getters['invoices/invoices'];
+      }
+    },
     data: () => {
       return {
         data: [
@@ -22,6 +36,18 @@
           { field: 'type' }, { field: 'status' }
         ]
       }
+    },
+
+    methods: {
+      getType(type) {
+        if(type ===  2){ return 'SELL'}
+
+        return 'BUY'
+      }
+    },
+
+    async fetch() {
+      await this.$store.dispatch('invoices/fetchInvoices');
     }
   };
 </script>
