d2d66eb1e4b431160ec9844c808c02a904febc20
Sarmat Khachirov <skhachirov12@gmail.com>
2020-06-19 17:49:09 +0300

    create token transfer functional & some contact utils

--- frontend/pages/exchange/transfer.vue
+++ frontend/pages/exchange/transfer.vue
@@ -4,19 +4,35 @@
       n-link(to="/exchange/trade" active-class="link--active").link.link--underlined.content-tabs-item Buy/sell
       n-link(to="/exchange/bills" active-class="link--active").link.link--underlined.content-tabs-item Bills
       n-link(to="/exchange/transfer" active-class="link--active").link.link--underlined.content-tabs-item Transfer
-    div.main-content Transfer
+    div.main-content
+      b-field
+        b-input(size="is-small" v-model="transferData.address" placeholder="Address")
+      b-field
+        b-input(size="is-small" type="number" v-model="transferData.amount" placeholder="Amount")
+      b-button(@click="transferFunds").btn.w-100 Transfer
+
 </template>
 
 <script>
-  export default {
-    name: "exchange-transfer",
-    layout: 'main',
-    data: () => {
-      return {
+export default {
+  name: "exchange-transfer",
+  layout: "main",
+  data: () => {
+    return {
+      transferData: {
+        address: "",
+        amount: 0
+      }
+    };
+  },
+  methods: {
+    transferFunds() {
+      if (this.transferData.address && this.transferData.amount > 0) {
+        this.$store.dispatch("contract/transferSimbaToken", this.transferData);
       }
     }
-  };
+  }
+};
 </script>
 
-<style lang="sass" scoped>
-</style>
+<style lang="sass" scoped></style>
