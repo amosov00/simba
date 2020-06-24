8c3d1e7492c9c1b5196a9f1c1eeda39aa110c6e0
Nikita Yugov <nikita@elastoo.com>
2020-06-24 16:09:30 +0700

    Changed deploy order

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -10,11 +10,6 @@ ssh "$SSH_USER"@"$SSH_HOST" "cd '$PROJECT_DIR' || mkdir -p '$PROJECT_DIR'"
 echo "########## Pull changes from gitlab repo ##########"
 ssh "$SSH_USER"@"$SSH_HOST" "cd '$PROJECT_DIR' && git pull $CI_REPOSITORY_URL $CI_COMMIT_REF_NAME"
 
-echo "########## Pull images from Gitlab Container Registry ##########"
-ssh "$SSH_USER"@"$SSH_HOST" "docker login -u '$CI_REGISTRY_USER' -p $CI_REGISTRY_PASSWORD $CI_REGISTRY"
-ssh "$SSH_USER"@"$SSH_HOST" "cd '$PROJECT_DIR' && docker-compose -f $DOCKER_COMPOSE_FILENAME pull"
-ssh "$SSH_USER"@"$SSH_HOST" "docker logout $CI_REGISTRY"
-
 echo "########## Copy .env files ##########"
 scp .env          "$SSH_USER"@"$SSH_HOST":"$PROJECT_DIR"
 scp .env.backend  "$SSH_USER"@"$SSH_HOST":"$PROJECT_DIR"
@@ -22,6 +17,10 @@ scp .env.frontend "$SSH_USER"@"$SSH_HOST":"$PROJECT_DIR"
 scp .env.db       "$SSH_USER"@"$SSH_HOST":"$PROJECT_DIR"
 scp .env.rabbitmq "$SSH_USER"@"$SSH_HOST":"$PROJECT_DIR"
 
+echo "########## Pull images from Gitlab Container Registry ##########"
+ssh "$SSH_USER"@"$SSH_HOST" "docker login -u '$CI_REGISTRY_USER' -p $CI_REGISTRY_PASSWORD $CI_REGISTRY"
+ssh "$SSH_USER"@"$SSH_HOST" "cd '$PROJECT_DIR' && docker-compose -f $DOCKER_COMPOSE_FILENAME pull"
+ssh "$SSH_USER"@"$SSH_HOST" "docker logout $CI_REGISTRY"
 
 echo "########## Reload containers services ##########"
 ssh "$SSH_USER"@"$SSH_HOST" "cd '$PROJECT_DIR' && docker-compose -f $DOCKER_COMPOSE_FILENAME up -d"
