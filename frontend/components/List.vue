2ce216fa79b911de33e51210291ee206dd9cb801
Sarmat Khachirov <skhachirov12@gmail.com>
2020-06-25 19:49:10 +0300

    list component

--- frontend/components/List.vue
+++ frontend/components/List.vue
@@ -1,13 +1,39 @@
-<template>
-  
+<template lang="pug">
+  ul.list
+    slot
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="scss">
+.list {
+  width: 100%;
+  border-radius: 0;
+  box-shadow: none;
 
+  &__item {
+    width: 100%;
+    padding: 6px 20px;
+    background-color: #efefef;
+    font-weight: 300;
+    font-size: 18px;
+    display: flex;
+    justify-content: space-between;
+
+    &:nth-child(1) {
+      font-weight: bold;
+    }
+
+    &:nth-child(2n) {
+      background-color: #fff;
+      padding: 14px 20px;
+    }
+
+    .blue {
+      color: #0060ff;
+    }
+  }
+}
 </style>
