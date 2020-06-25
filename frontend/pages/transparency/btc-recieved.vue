f6011f972242cd124c560993e040263bf832aac9
Sarmat Khachirov <skhachirov12@gmail.com>
2020-06-25 19:48:07 +0300

    transparency page

---
+++ frontend/pages/transparency/btc-recieved.vue
@@ -0,0 +1,178 @@
+<template lang="pug">
+  div
+    div.main-content
+      h1.title.is-size-4 Transparency
+      p.title.is-size-5 Received by Storage
+      section
+        b-table(:data="tableData" hoverable striped)
+          template(slot-scope="props")
+            b-table-column(field="date" label="Date" sortable width="50") {{props.row.date}}
+            b-table-column(field="txHash" label="TxHash" width="70").has-text-primary.overflow-reset
+              b-tooltip(:label="props.row.transactionHash" type="is-black" position="is-bottom").w-100
+                a(:href="'https://etherscan.io/tx/' + props.row.transactionHash" target="_blank").text-clamp {{ props.row.transactionHash }}
+            b-table-column(field="amount" label="Amount, BTC" width="100") {{props.row.amount}}
+        div.text-center
+          button.btn--outlined.mt-4 more
+</template>
+
+<script>
+export default {
+  name: "contacts",
+  layout: "main",
+  data() {
+    return {};
+  },
+  computed: {
+    tableData() {
+      return [
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        },
+        {
+          date: "01/06/2020, 09:49:15",
+          transactionHash:
+            "3f095787422746cd026f5a49974096083fe0b3c47e209ed0d1988b1b16fe35b6",
+          amount: 116.1005
+        }
+      ];
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped></style>
---
+++ frontend/pages/transparency/index.vue
@@ -0,0 +1,109 @@
+<template lang="pug">
+  div.transparency
+    div.main-content
+      h1.title.is-size-4 Transparency
+        section.transparency__section
+          img(src="../../assets/images/chart.png")
+      h1.title.is-size-4 Current Balances
+        section.transparency__section
+          img(src="../../assets/images/logo-lg.png")
+          List.mt-3
+            li.list__item(v-for="(total, i) in simbaTotals" :key="i")
+              span.list__item--name {{total.name}}
+              span.list__item--value {{total.value}}
+          img(src="../../assets/images/bitcoin-lg.svg")
+          List.mt-3
+            li.list__item(v-for="(total, i) in btcTotals" :key="i")
+              span.list__item--name {{total.name}}
+              span.list__item--value {{total.value}}
+</template>
+
+<script>
+import List from "~/components/List";
+export default {
+  name: "transparency",
+  layout: "main",
+  components: {
+    List
+  },
+  data() {
+    return {
+      simbaTotals: [
+        {
+          name: "Total Assets: ",
+          value: "500,000,000 SIMBA"
+        },
+        {
+          name: "Total USD equivalent: ",
+          value: "50,000.78 SIMBA"
+        },
+        {
+          name: "Issued: ",
+          value: "850,000,000 SIMBA"
+        },
+        {
+          name: "Redeemed: ",
+          value: "350,000,000 SIMBA"
+        },
+        {
+          name: "Quarantined: ",
+          value: "10,000,000 SIMBA"
+        },
+        {
+          name: "Circulation: ",
+          value: "490,000,000 SIMBA"
+        },
+        {
+          name: "SIMBA holders: ",
+          value: "574"
+        },
+        {
+          name: "Ethereum Contract: ",
+          value:
+            "SIMBA Stablecoin (SIMBA)0x7806a1b2b6056cda57d3e889a9513615733e2b"
+        }
+      ],
+      btcTotals: [
+        {
+          name: "Total Assets: ",
+          value: "14,847.8056 BTC"
+        },
+        {
+          name: "Received: ",
+          value: "18,357.9056 BTC"
+        },
+        {
+          name: "Paid out: ",
+          value: "3,510.1000 BTC"
+        },
+        {
+          name: "Liechtenstein: ",
+          value: " 3,012.0051 BTC"
+        },
+        {
+          name: "UAE: ",
+          value: "1,531.8001 BTC"
+        },
+        {
+          name: "New Zealand: ",
+          value: "8,152.0002 BTC"
+        },
+        {
+          name: "Switzerland: ",
+          value: "2,152.0002 BTC"
+        }
+      ]
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.transparency {
+  &__section {
+    margin-top: 50px;
+    text-align: center;
+  }
+}
+</style>
