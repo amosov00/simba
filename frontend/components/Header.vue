263d49d5c780b4fed7f690cc8bc709cc843e0edf
saint777 <saint7794@gmail.com>
2020-06-30 16:58:57 +0300

    feat(profile): profile dropdown

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -8,8 +8,7 @@
           div.logo-text SIMBA
           div.logo-subtext Swiss Quality Stablecoin
       div.column.is-6.has-text-right(v-if="user")
-        div.mb-1.text-large
-          nuxt-link(to="/profile/data/").link {{ user.first_name }} {{ user.last_name }}
+        ProfileDropdown(:name="`${user.first_name} ${user.last_name}`")
         div.has-text-weight-bold.text-large 999 000 050 000 SIMBA
     div.header-menu.columns.is-flex(v-if="user")
       div.column.is-8
@@ -18,8 +17,15 @@
 </template>
 
 <script>
+  import ProfileDropdown from "~/components/ProfileDropdown";
+
   export default {
     name: 'Header',
+
+    components: {
+      ProfileDropdown
+    },
+
     computed: {
       user() {
         return this.$store.getters.user;
