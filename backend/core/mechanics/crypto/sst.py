4adc1c112979534c7d5163449a9e48499c2028aa
danya <daniil.kuvaldin@elastoo.com>
2020-07-06 19:19:08 +0300

    Fix sending sst to referrals

--- backend/core/mechanics/crypto/sst.py
+++ backend/core/mechanics/crypto/sst.py
@@ -19,7 +19,9 @@ class SSTWrapper(CryptoValidation, CryptoCurrencyRate):
     PERIOD: int = 2500000
 
     def __init__(self):
-        self.api_wrapper = FunctionsContractWrapper(SST_CONTRACT, SIMBA_ADMIN_ADDRESS, SIMBA_ADMIN_PRIVATE_KEY)
+        self.api_wrapper = FunctionsContractWrapper(
+            SST_CONTRACT, SIMBA_ADMIN_ADDRESS, SIMBA_ADMIN_PRIVATE_KEY
+        )
 
     @classmethod
     def _calculate_referrals_accurals(cls, ref_level: int, sst_tokens: int) -> int:
@@ -40,10 +42,17 @@ class SSTWrapper(CryptoValidation, CryptoCurrencyRate):
         sst_tokens = self.simba_to_sst(simba_tokens)
         referral = await ReferralCRUD.find_by_user_id(user.id)
         for i in range(1, 6):
-            user = await UserCRUD.find_by_id(ObjectId(referral[f"ref{i}"]))
-            wallet: str = user["user_eth_addresses"][0] if len(user["user_eth_addresses"]) > 0 else None
+            if not referral[f"ref{i}"]:
+                continue
+            current_user = await UserCRUD.find_by_id(ObjectId(referral[f"ref{i}"]))
+            wallet: str = current_user["user_eth_addresses"][0] if (
+                "user_eth_addresses" in current_user
+                and len(current_user["user_eth_addresses"]) > 0
+            ) else None
             if wallet is not None:
                 self.api_wrapper.freeze_and_transfer(
-                    wallet, self._calculate_referrals_accurals(i, sst_tokens), self.PERIOD
+                    wallet,
+                    self._calculate_referrals_accurals(i, sst_tokens),
+                    self.PERIOD,
                 )
         return True
