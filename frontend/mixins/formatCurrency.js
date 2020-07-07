702c00a6eb86ea2e0354b1773d284ff12d93fd58
Sarmat Khachirov <skhachirov12@gmail.com>
2020-07-07 16:57:53 +0300

    improve balance selector | replace into own component, add icons color state

--- frontend/mixins/formatCurrency.js
+++ frontend/mixins/formatCurrency.js
@@ -2,6 +2,9 @@ export default {
   methods: {
     simbaFormat(value) {
       return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+    btcFormat(value) {
+      return value.toFixed(4)
     }
   }
 }
