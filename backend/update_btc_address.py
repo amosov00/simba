e915f1be153ee84e9b33660c96a999083d32019b
Nikita Yugov <nikita@elastoo.com>
2020-07-14 22:42:42 +0700

    Changed default BTC_COLD_XPUB_UAE to BTC_COLD_XPUB_SWISS

--- backend/update_btc_address.py
+++ backend/update_btc_address.py
@@ -1,12 +1,12 @@
 import asyncio
 
 from database.crud import BTCAddressCRUD
-from config.crypto import BTC_COLD_XPUB_UAE, BTC_HOT_WALLET_ADDRESS
+from config.crypto import BTC_COLD_XPUB_SWISS, BTC_HOT_WALLET_ADDRESS
 
 
 async def main():
     await BTCAddressCRUD.update_many({"address": {"$ne": BTC_HOT_WALLET_ADDRESS}},
-                                     {"cold_wallet_title": BTC_COLD_XPUB_UAE.title})
+                                     {"cold_wallet_title": BTC_COLD_XPUB_SWISS.title})
 
 
 if __name__ == '__main__':
