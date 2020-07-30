741fc8b06a9f5ee75780fb5001a55a3a075593dc
elastoo_saint777 <alexander.hegai@elastoo.com>
2020-07-30 12:42:04 +0300

    fix(admin-invoices/users): added search for invoices, trim to search query, localization

--- frontend/pages/invoices/index.vue
+++ frontend/pages/invoices/index.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
   div.main-content
     h1.title.is-size-4 {{$t('su_invoices.invoices')}}
-    b-table(:data="invoices" paginated per-page="20" default-sort="created_at" default-sort-direction="desc")
+    div.mb-3
+      b-input(v-model="searchQuery" @input="onSearchInput" :placeholder="`${this.$i18n.t('other.search')}...`" icon="magnify")
+    div(v-if="invoicesToView.length <= 0") {{ $t('other.search_empty_results') }}
+    b-table(v-if="invoicesToView.length > 0" :data="invoicesToView" paginated per-page="20" default-sort="created_at" default-sort-direction="desc")
       template(slot-scope="props")
         b-table-column(field="created_at" label="Date" width="150" sortable) {{ timestampFromUtc(props.row.created_at) }}
         b-table-column(field="_id" label="ID" width="50" sortable)
@@ -19,12 +22,39 @@
   import formatCurrency from "~/mixins/formatCurrency";
 
   import moment from 'moment'
+  import _ from 'lodash'
 
   export default {
     name: "invoices",
     layout: "main",
     middleware: ["adminRequired"],
     mixins: [formatDate, formatCurrency],
+
+    data: () => ({
+      invoicesCache: [],
+      invoicesToView: [],
+      searchQuery: '',
+    }),
+
+    methods: {
+      onSearchInput: _.debounce(function() {
+        let properSearchQuery = this.searchQuery.toLowerCase().trim()
+
+        if(properSearchQuery !== '' || properSearchQuery.length <= 0) {
+          this.invoicesToView = this.invoicesCache.filter(el => {
+            for(let key in el){
+              if(String(el[key]).toLowerCase().includes(properSearchQuery)) {
+                return true
+              }
+            }
+            return false
+          })
+        } else {
+          this.invoicesToView = this.invoicesCache
+        }
+      }, 500)
+    },
+
     async asyncData({store}) {
       const res = await store.dispatch('invoices/fetchAdminInvoices')
 
@@ -33,7 +63,8 @@
       })
 
       return {
-        invoices: res
+        invoicesCache: res,
+        invoicesToView: res
       }
     }
   };
