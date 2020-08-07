24fac9d19934b1aade719b60528ad6736c826fe4
Nikita Yugov <nikita@elastoo.com>
2020-08-07 14:47:05 +0300

    removed single scripts

--- backend/sync_btc_wallets.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-from celery_app.tasks import fetch_empty_btc_addresses_info
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(fetch_empty_btc_addresses_info())
