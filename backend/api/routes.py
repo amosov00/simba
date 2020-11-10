6a0e45eaa5385226df97908202ca1e61062d4d06
Nikita Yugov <nikita@elastoo.com>
2020-11-10 10:17:19 +0300

    added ping endpoint

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from config import DEBUG
 
-from api.controllers import account, debug, meta, invoice, transparency, admin
+from api.controllers import account, debug, meta, invoice, transparency, admin, ping
 from api.dependencies import user_is_superuser
 
 api_router = APIRouter()
@@ -12,6 +12,7 @@ api_router.include_router(account.router, prefix="/account", tags=["account"])
 api_router.include_router(meta.router, prefix="/meta", tags=["meta"])
 api_router.include_router(invoice.router, prefix="/invoices", tags=["invoice"])
 api_router.include_router(transparency.router, prefix="/transparency", tags=["transparency"])
+api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
 api_router.include_router(
     admin.admin_router,
     prefix="/admin",
