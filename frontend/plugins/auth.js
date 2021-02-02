fd3a0c08a241ea1c4e77f206fff1c0979817e0b1
Nikita Yugov <nikita@elastoo.com>
2021-02-02 22:32:52 +0300

    added domain to cookies, fixing cookies

--- frontend/plugins/auth.js
+++ frontend/plugins/auth.js
@@ -1,50 +1,56 @@
-export default ({ app, redirect, route }, inject) => {
-	inject('authLogin', async (email, password, pin_code) => {
-		return await app.$axios.post('/account/login/',
-			{
-				email: email,
-				password: password,
-				pin_code: pin_code
-			}
-		).then(resp => {
-			app.store.commit('setUser', resp.data.user);
-			app.$axios.setToken(resp.data.token, 'Bearer');
-			app.$cookies.set('token', resp.data.token, {
-				path: '/',
-				maxAge: 60 * 60 * 24 * 7,
-			});
-			redirect('/exchange');
-
-			// window.location.href = '/exchange/'; Что это?
-			return true;
-		}).catch(resp => {
-		  return resp;
+export default ({app, redirect, route, $config}, inject) => {
+  inject('authLogin', async (email, password, pin_code) => {
+    return await app.$axios.post('/account/login/',
+      {
+        email: email,
+        password: password,
+        pin_code: pin_code
+      }
+    ).then(resp => {
+      app.store.commit('setUser', resp.data.user);
+      app.$axios.setToken(resp.data.token, 'Bearer');
+      app.$cookies.set('token', resp.data.token, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7,
+        domain: $config.domain,
+      });
+      redirect('/exchange');
+      return true;
+    }).catch(resp => {
+      return resp;
     })
-	});
-	inject('authLogout', () => {
-		app.store.commit('deleteUser');
-		app.$axios.setToken(null);
-		app.$cookies.remove('token');
-		window.location.href = '/'
-	});
-	inject('authFetchUser', async () => {
-		let {data, status} = await app.$axios.get('/account/user/');
-		if (status === 200) {
-			app.store.commit('setUser', data)
-		} else {
-			redirect('/');
-		}
-	});
-	inject('userIsLoggedIn', () => {
-		return app.store.getters.user
-	});
-	inject('userIsStaff', () => {
-		return app.store.getters.user && app.store.getters.user.is_staff
-	});
-	inject('userIsSuperuser', () => {
-		if (app.store.getters.user) {
-			return app.store.getters.user.is_superuser
-		}
-		return false
-	});
+  });
+  inject('authLogout', () => {
+    app.store.commit('deleteUser');
+    app.$axios.setToken(null);
+    app.$cookies.remove('token', {
+      domain: $config.domain,
+    });
+    redirect('/');
+  });
+  inject('authFetchUser', async () => {
+    return app.$axios.get('/account/user/',)
+      .then(resp => {
+        let {status, data} = resp;
+        if (status === 200) {
+          app.store.commit('setUser', data)
+        } else {
+          redirect('/');
+        }
+      }).catch(err => {
+        console.error(err)
+      })
+  });
+  inject('userIsLoggedIn', () => {
+    return app.store.getters.user
+  });
+  inject('userIsStaff', () => {
+    return app.store.getters.user && app.store.getters.user.is_staff
+  });
+  inject('userIsSuperuser', () => {
+    if (app.store.getters.user) {
+      return app.store.getters.user.is_superuser
+    }
+    return false
+  });
 }
--- rundev-front.2.sh
+++ rundev-front.2.sh
@@ -3,5 +3,6 @@ export ENV=development
 export NODE_ENV=development
 export API_URL=https://simba-dev.elastoo.com/api
 export API_URL_BROWSER=https://simba-dev.elastoo.com/api
+export DOMAIN=simba-dev.elastoo.com
 
 cd frontend && npm run dev
