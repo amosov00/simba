6becbfdc16d26c0c3199808ac8a8f081e0e79f24
themashina <amosov00@gmail.com>
2021-02-23 19:56:48 +0200

    commit

--- frontend/pages/profile/verification.vue
+++ frontend/pages/profile/verification.vue
@@ -1,25 +1,20 @@
-<template lang="pug">
-  div
-    div#sumsub-websdk-container
+<template>
+  <div>
+    <div id="sumsub-websdk-container"></div>
+    <div>{{currentStep}}</div>
+  </div>
 </template>
 
 <script>
 import snsWebSdk from '@sumsub/websdk'
+import { sumSubStyles } from '~/consts'
 export default {
   name: 'profile-verification',
   layout: 'profile',
   computed: {},
   data: () => ({
     token: null,
-    steps: {
-      current: 'profile.email_verification',
-      list: [
-        'profile.email_verification',
-        'profile.verify_address',
-        'profile.id_verification',
-        'profile.source_of_funds_verification',
-      ],
-    },
+    currentStep: ''
   }),
   methods: {
     launchWebSdk(apiUrl, flowName, accessToken, applicantEmail, applicantPhone) {
@@ -34,6 +29,12 @@ export default {
           phone: applicantPhone,
           onMessage: (type, payload) => {
             console.log('WebSDK onMessage', type, payload)
+            if (type === 'idCheck.onStepInitiated') {
+              this.currentStep = payload.idDocSetType
+            }
+          },
+          uiConf: {
+            customCssStr: sumSubStyles
           },
           onError: (error) => {
             console.error('WebSDK onError', error)
@@ -42,22 +43,6 @@ export default {
         .build()
       snsWebSdkInstance.launch('#sumsub-websdk-container', 'basic-kyc')
     },
-    activeStep(i) {
-      if (this.failedStep(i)) {
-        return false
-      }
-      return i < this.steps.list.indexOf(this.steps.current) + 1
-    },
-
-    failedStep(i) {
-      if (this.stepFail) {
-        if (i === this.stepFail) {
-          return true
-        }
-      }
-
-      return false
-    },
   },
   async mounted() {
     let { data } = await this.$axios.get('/account/kyc/token/')
@@ -65,8 +50,5 @@ export default {
     // 'tst:Dt2mTU9SnHl9SGjALc5hhCMe.L4VJ9g2XHJbYjipw5hI39QZd7amHIzMo'
     this.launchWebSdk('https://test-api.sumsub.com', 'basic-kyc', this.token)
   },
-  async asyncData({ store }) {},
 }
 </script>
-
-<style lang="sass" scoped></style>
